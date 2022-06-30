7cdf0abaf1a8e714655c091bc8f0f44b088093a8
Pavel Sakhanko <iospaveldev@gmail.com>
2022-06-30 07:34:45 +0200

    init

---
+++ gaTest/gaTest/gaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gaTestApp.swift
+//  gaTest
+//
+//  Created by Pavel Sakhanko on 30/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct gaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
